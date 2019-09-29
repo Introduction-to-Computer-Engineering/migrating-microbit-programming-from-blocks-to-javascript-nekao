04d821238250ac574e287799d3df4dd0139318b5
Denika <denikao@hotmail.com>
2019-09-29 15:48:45 -0600

    Update 2HappyFaceSadFace.js

---
+++ 2HappyFaceSadFace.js
@@ -0,0 +1,19 @@
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+basic.clearScreen()
