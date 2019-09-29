50fe0bc8acf9644fb4551970306c098d1c4989ea
Denika <denikao@hotmail.com>
2019-09-29 15:47:25 -0600

    Update 10MorseCode.js

---
+++ 10MorseCode.js
@@ -0,0 +1,42 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+    } else if (receivedNumber == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+    } else if (receivedNumber == 2) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+    basic.pause(100)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(2)
+})
+radio.setGroup(1)
+basic.showString("Morse Code")
